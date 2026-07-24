8cad3c2ac14cdba096a91393ae3eaeb98e3a4604
Jimyty1 <Jimyty124@gmail.com>
2026-07-24 11:50:33 +0200

    Created .gitignore and starting with the TDD process to create the first algorithm (bubble sort).

---
+++ tests.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <assert.h>
+
+void bubble_sort_test(){
+    // Test cases for bubble sort
+    int result = 0;
+    assert(result == 0);
+}
+
+int main() {
+    bubble_sort_test();
+    printf("All tests passed!\n");
+    return 0;
+}
