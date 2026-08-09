c9daa480b887cd390ca4913f355a70b6e66c0f56
Jimyty1 <Jimyty124@gmail.com>
2026-08-09 17:01:17 +0100

    moved the get element and get description functions to util files to keep arrVisualizer clean. Also added a maxValue variable which is being used to normalize the size of the added bars. These make for a cleaner visualization of the array and the changes on the array. The color from the css is now bound to the array bar selector which also helps the visualization. Following this some changes have to be made to the animations for quicksort, mergesort and heapsort as they look confusing. Although for now there is a working product many changes need to made to the animation logic. This is to make it more intuitive and facilitate learning from this resource.

--- frontend/src/components/ArrVisualizer.tsx
+++ frontend/src/components/ArrVisualizer.tsx
@@ -1,156 +1,53 @@
 import type { AlgorithmEvent } from "../types/algorithm";
 import "../css/ArrVisualizer.css";
 import { useState } from "react";
+import { getEventDescription } from "../utils/getEventDescription";
+import { getElementState } from "../utils/getElementState";
 
 export default function ArrVisualizer({events, currentEventIndex, }: {events: AlgorithmEvent[];currentEventIndex: number;})
 {
   const currentEvent = events[currentEventIndex] ?? null;
-  const [isAnimating, setIsAnimating] = useState(false);
+  const maxValue = currentEvent
+    ? Math.max(...currentEvent.array): 1;
 
-  function getEventDescription(event: AlgorithmEvent) {
-    switch (event.type) {
-        case "compare":
-            return `Comparing positions ${event.i} and ${event.j}`;
-
-        case "compare_value":
-            return `Comparing position ${event.i} with ${event.value}`;
-
-        case "swap":
-            return `Swapping positions ${event.i} and ${event.j}`;
-
-        case "overwrite":
-            return `Writing ${event.with_value} to position ${event.index}`;
-
-        case "pivot":
-            return `Selecting ${event.array[event.index]} as pivot`;
-
-        case "pivot_placed":
-            return `Pivot placed at position ${event.index}`;
-
-        case "merge_start":
-            return `Merging positions ${event.start}–${event.end}`;
-
-        case "merge_compare":
-            return `Comparing ${event.left} and ${event.right}`;
-
-        case "heapify":
-            return `Heapifying position ${event.root}`;
-
-        case "done":
-            return "Sorting complete";
-    }
-}
-  function getElementState(event: AlgorithmEvent | null, index: number) {
-    if (!event) {
-      return "";
-    }
-
-    switch (event.type) {
-      case "partition": {
-        const classes: string[] = [];
-        
-        if (index < event.start || index > event.end) {
-          classes.push("dimmed");
-          return classes.join(" ");
-        }
-        classes.push("partition_range");
-
-        if (index === event.j) {
-          classes.push("j_index");
-        }
-
-        if (index === event.boundary) {
-          classes.push("boundary");
-        }
-
-        if (index === event.pivot) {
-          classes.push("pivot");
-        }
-
-        return classes.join(" ");
-      }
-
-      case "compare":
-        if (index === event.i || index === event.j) {
-          return "compared";
-        }
-        break;
-      case "compare_value":
-        if (index === event.i) {
-          return "compare_value";
-        }
-        break;
-
-      case "swap":
-        if (index === event.i || index === event.j) {
-          return "swapped";
-        }
-        break;
-
-      case "pivot":
-        if (index === event.index) {
-          return "pivot";
-        }
-        break;
-
-      case "overwrite":
-        if (index === event.index) {
-          return "overwritten";
-        }
-        break;
-
-      case "heapify":
-        if (
-          index === event.root ||
-          index === event.left ||
-          index === event.right
-        ) {
-          return "heapified";
-        }
-        break;
-
-      case "merge_compare":
-        if (index === event.left || index === event.right) {
-          return "merge_compared";
-        }
-        break;
-
-      case "merge_start":
-        if (index >= event.start && index <= event.end) {
-          return "merge_range";
-        }
-
-        return "dimmed";
-
-      case "pivot_placed":
-        if (index === event.index) {
-          return "pivot_placed";
-        }
-        break;
-
-      case "done":
-        return "done";
-    }
-
-    return "";
-  }
-
+  
   return (
     <div className="visualArray">
       {currentEvent ? (
         <div className="eventDetails">
           <div className="arrayValues">
-            {currentEvent.array.map((value, index) => (
-              <span
+    {currentEvent.array.map((value, index) => {
+        const barHeight = (value / maxValue) * 300;
+
+        return (
+            <div
                 key={index}
-                className={`arrayValue ${getElementState(currentEvent, index)}`}
-              >
-                {value}
-              </span>
-            ))}
-          </div>
+                className={`arrayElement ${getElementState(
+                    currentEvent,
+                    index
+                )}`}
+            >
+                <div
+                    className="arrayBar"
+                    style={{
+                        height: `${barHeight}px`,
+                    }}
+                />
+
+                <div className="arrayValue">
+                    {value}
+                </div>
+
+                <div className="arrayIndex">
+                    {index}
+                </div>
+            </div>
+        );
+    })}
+</div>
 
           <h4>{JSON.stringify(currentEvent, null, 2)}</h4>
+           <h4>{getEventDescription(currentEvent)}</h4>
         </div>
       ) : (
         <p>No events available.</p>
