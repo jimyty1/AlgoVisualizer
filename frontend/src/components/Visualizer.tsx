02de6ec3194f53bb58dd42930805669de9c37900
Jimyty1 <Jimyty124@gmail.com>
2026-08-09 13:07:32 +0100

    made an input for arrays and algorithms that allows the user to change the array and the algo in real time. These values are in the useEffect dependency array and therefore refresh the call to the backend on change.

--- frontend/src/components/Visualizer.tsx
+++ frontend/src/components/Visualizer.tsx
@@ -9,6 +9,8 @@ import ArrVisualizer from "./ArrVisualizer";
 function Visualizer() {
   const [events, setEvents] = useState<AlgorithmEvent[]>([]);
   const [currentStep, setCurrentStep] = useState(0);
+  const [algo, setAlgo] = useState("bubble");
+  const [arrayInput, setArrayInput] = useState([64,34,25,12,22,11,90]);
 
   useEffect(() => {
     fetch("/test", {
@@ -17,8 +19,8 @@ function Visualizer() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        algorithm: "quick",
-        array: [64, 34, 25, 12, 22, 11, 90],
+        algorithm: algo,
+        array: arrayInput,
       }),
     })
       .then((response) => response.text())
@@ -35,7 +37,7 @@ function Visualizer() {
         setEvents([]);
         setCurrentStep(0);
       });
-  }, []);
+  }, [algo, arrayInput]);
 
   const maxStep = Math.max(events.length - 1, 0);
   const currentEvent = events[currentStep] ?? null;
@@ -45,9 +47,22 @@ function Visualizer() {
       <h1>Algo Visualizer</h1>
 
       <h2>Test</h2>
+      <select name="algorithm" onChange={(e) => {setAlgo(e.target.value);}}>
+        <option value="bubble">Bubble Sort</option>
+        <option value="selection">Selection Sort</option>
+        <option value="insertion">Insertion Sort</option>
+        <option value="merge">Merge Sort</option>
+        <option value="quick">Quick Sort</option>
+        <option value="heap">Heap Sort</option>
+      </select>
+      <br/>
+      <input name="array" type="text" placeholder="Enter array (comma-separated)" 
+      defaultValue="64,34,25,12,22,11,90" 
+      onChange={(e) => {setArrayInput(e.target.value.split(",").map(Number));}} />
+
+      <span style={{ marginTop: "1rem", height: "1rem", display: "block" }}/>
 
       <p>Output:</p>
-
       <ArrVisualizer currentEvent={currentEvent} />
       <Controller step={currentStep} maxStep={maxStep} onStep={setCurrentStep} />
     </>
